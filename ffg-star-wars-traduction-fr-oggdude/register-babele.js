895d24879142b56f1fdb8f6ce25e9fdd61ad5c54
spothieux <spothieux@gmail.com>
2023-06-01 23:48:43 +0200

    Fix version with v10 compatibility

--- ffg-star-wars-traduction-fr-oggdude/register-babele.js
+++ ffg-star-wars-traduction-fr-oggdude/register-babele.js
@@ -1,6 +1,6 @@
 Hooks.on('init', () => {
     if(typeof Babele !== 'undefined') {
-		if(isNewerVersion(game.version, 9))
+		if(isNewerVersion(game.version, 10))
 		{
 			Babele.get().register({
 				module: 'ffg-star-wars-traduction-fr-oggdude',
