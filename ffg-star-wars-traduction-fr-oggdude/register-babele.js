49ebf055619a4046e6a2888249f0b62c686cb04b
spothieux <spothieux@gmail.com>
2024-12-14 22:40:27 +0100

    Remove support of foundry V9, support foundry V12 only
    Remove support of foundry V9
    Support foundry V12 only
    Remove access to depecrated function

--- ffg-star-wars-traduction-fr-oggdude/register-babele.js
+++ ffg-star-wars-traduction-fr-oggdude/register-babele.js
@@ -1,20 +1,9 @@
 Hooks.on('init', () => {
     if(typeof Babele !== 'undefined') {
-		if(isNewerVersion(game.version, 10))
-		{
-			Babele.get().register({
+		game.babele.register({
 				module: 'ffg-star-wars-traduction-fr-oggdude',
 				lang: 'fr',
 				dir: 'compendium'
-			});
-		}
-		else
-		{
-			Babele.get().register({
-				module: 'ffg-star-wars-traduction-fr-oggdude',
-				lang: 'fr',
-				dir: 'compendiumV9'
-			});
-		}			
+			});			
     }
 });
